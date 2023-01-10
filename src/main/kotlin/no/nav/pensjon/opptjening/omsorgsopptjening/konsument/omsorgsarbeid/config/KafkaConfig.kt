81f6eecbe1cb862ff0149c82ed537b0785f349d9
Mo Amini <mo.amini@nav.no>
2023-01-10 13:41:07 +0100

    Lagt til kafka support

---
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/konsument/omsorgsarbeid/config/KafkaConfig.kt
@@ -0,0 +1,66 @@
+package no.nav.pensjon.opptjening.omsorgsopptjening.konsument.omsorgsarbeid.config
+
+
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.listener.ContainerProperties
+import java.time.Duration
+
+
+@EnableKafka
+@Profile("test", "prod")
+@Configuration
+class KafkaConfig(
+    @param:Value("\${kafka.keystore.path}") private val keystorePath: String,
+    @param:Value("\${kafka.credstore.password}") private val credstorePassword: String,
+    @param:Value("\${kafka.truststore.path}") private val truststorePath: String,
+    @param:Value("\${kafka.brokers}") private val aivenBootstrapServers: String,
+    @param:Value("\${kafka.security.protocol}") private val securityProtocol: String,
+    @Autowired private val kafkaErrorHandler: KafkaStoppingErrorHandler?
+    ) {
+
+    @Bean
+    fun sedKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String>? {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
+        factory.consumerFactory = kafkaConsumerFactory()
+        factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL
+        factory.containerProperties.setAuthExceptionRetryInterval( Duration.ofSeconds(4L) )
+
+        if (kafkaErrorHandler != null) {
+            factory.setCommonErrorHandler(kafkaErrorHandler)
+        }
+        return factory
+    }
+
+    fun kafkaConsumerFactory(): ConsumerFactory<String, String> {
+        val configMap: MutableMap<String, Any> = HashMap()
+        //Sikkerhet config
+        configMap[SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG] = keystorePath
+        configMap[SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG] = credstorePassword
+        configMap[SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG] = credstorePassword
+        configMap[SslConfigs.SSL_KEY_PASSWORD_CONFIG] = credstorePassword
+        configMap[SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG] = "JKS"
+        configMap[SslConfigs.SSL_KEYSTORE_TYPE_CONFIG] = "PKCS12"
+        configMap[SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG] = truststorePath
+        configMap[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = securityProtocol
+        //Konsument config
+        configMap[ConsumerConfig.CLIENT_ID_CONFIG] = "omsorgsopptjening-konsument-omsorgsarbeid"
+        configMap[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = aivenBootstrapServers
+        configMap[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
+        configMap[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+        configMap[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = 1
+
+        return DefaultKafkaConsumerFactory(configMap, StringDeserializer(), StringDeserializer())
+    }
+}
---
+++ src/test/kotlin/no/nav/pensjon/opptjening/omsorgsarbeid/konsument/omsorgsarbeid/SpringAppTest.kt
@@ -0,0 +1,17 @@
+package no.nav.pensjon.opptjening.omsorgsarbeid.konsument.omsorgsarbeid
+
+import no.nav.pensjon.opptjening.omsorgsarbeid.konsument.omsorgsarbeid.config.IntegrationtestConfig
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest(classes = [ IntegrationtestConfig::class])
+@EmbeddedKafka(count = 1, controlledShutdown = true, brokerProperties= ["log.dir=out/embedded-kafkainnsyn"])
+@ActiveProfiles("integrationtest")
+class SpringAppTest {
+
+    @Test
+    fun StartApp() {
+    }
+}
