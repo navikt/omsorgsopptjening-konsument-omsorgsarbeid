84fb29eeefb3116b9b09ebd25e05369bdbfd199b
eivind <eivind.askestad@nav.no>
2023-01-20 10:08:05 +0100

    Updated ar to omsorgsAr in OmsorgsarbeidModel
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/konsument/omsorgsarbeid/omsorg/OmsorgsarbeidModel.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/konsument/omsorgsarbeid/omsorg/OmsorgsarbeidModel.kt
@@ -2,9 +2,9 @@ package no.nav.pensjon.opptjening.omsorgsopptjening.konsument.omsorgsarbeid.omso
 
 import java.time.LocalDate
 
-data class OmsorgsArbeidKey(val omsorgsyterFnr: String, val ar: String)
-data class OmsorgsArbeid(val omsorgsyter: Omsorgsyter, val ar: String, val hash: String)
+data class OmsorgsArbeid(val omsorgsyter: Omsorgsyter, val omsorgsAr: String, val hash: String)
 data class Omsorgsyter(val fnr: String, val utbetalingsperioder: List<UtbetalingsPeriode>)
 data class UtbetalingsPeriode(val fom: LocalDate, val tom: LocalDate, val omsorgsmottaker: OmsorgsMottaker)
 data class OmsorgsMottaker(val fnr: String)
 
+data class OmsorgsArbeidKey(val omsorgsyterFnr: String, val omsorgsAr: String)
