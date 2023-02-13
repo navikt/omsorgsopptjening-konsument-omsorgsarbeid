088aad80980eff353d1a1f88baaef588a82d75c9
Mo Amini <mo.amini@nav.no>
2023-02-13 15:19:30 +0100

    Tar i bruk omsorgsopptjening-domene-lib Sletter lokale domeneklasser
    Co-authored-by: eivind.askestad@nav.no

--- src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/konsument/omsorgsarbeid/omsorg/OmsorgopptjeningProducer.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/omsorgsopptjening/konsument/omsorgsarbeid/omsorg/OmsorgopptjeningProducer.kt
@@ -1,5 +1,10 @@
 package no.nav.pensjon.opptjening.omsorgsopptjening.konsument.omsorgsarbeid.omsorg
 
+import no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene.kafka.KafkaHeaderKey.Companion.MESSAGE_TYPE
+import no.nav.pensjon.opptjening.omsorgsopptjening.felles.domene.kafka.KafkaMessageType
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.header.Header
+import org.apache.kafka.common.header.internals.RecordHeader
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
@@ -12,8 +17,11 @@ class OmsorgopptjeningProducer(
 ) {
 
     fun send(key: String, value: String) {
+
+        val headers : List<Header> = mutableListOf(RecordHeader(MESSAGE_TYPE,KafkaMessageType.OMSORGSARBEID.name.encodeToByteArray()))
+        val pr = ProducerRecord(omsorgsOpptjeningTopic, null, null, key, value, headers)
         kafkaProducer
-            .send(omsorgsOpptjeningTopic, key, value)
+            .send(pr)
             .get(1, TimeUnit.SECONDS)
     }
 }
